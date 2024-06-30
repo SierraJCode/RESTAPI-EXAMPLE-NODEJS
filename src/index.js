14f6d9b2b74103f6646fa6e340b3302466915e8b
Jesus Sierra <sierraja06@gmail.com>
2024-06-29 22:05:33 -0500

    Aplicacion funcionando en el puerto 4000

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express();
+
+app.set('port', 4000);
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+});
